a1d740b83e8c6c4f52e1d93f409abab1942943cd
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-07-19 11:45:21 +0100

    note

--- ex2.js
+++ ex2.js
@@ -2,3 +2,5 @@ var isCoolNumber = require(process.argv[2]);
 var assert = require('assert');
 
 assert.strictEqual(isCoolNumber(42), true, '42 should return "true"');
+
+// tsk. naughty solution only testing for a truth*y* return. 
